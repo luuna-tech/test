00d6460b9666e768e3860ee441ada6a85d05edc5
vixrodriguez <206ultimate>
2022-06-28 19:21:45 -0500

    [UPD] Creating tables

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,48 @@
 from flask import Flask
-server = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
 
-@server.route("/")
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5433/zebrands"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class Brands(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    products = db.relationship("Products")
+
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    sku = db.Column(db.String)
+    price = db.Column(db.Float, nullable=False)
+    brand_id = db.Column(db.Integer, db.ForeignKey("brands.id"))
+
+
+class UsersTypes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    user_type_id = db.Column(db.Integer, db.ForeignKey("users_types.id"))
+
+
+db.create_all()
+
+
+@app.route("/")
 def hello():
     return "Hello World!"
 
 
 if __name__ == "__main__":
-    server.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
