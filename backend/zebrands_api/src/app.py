6abb87a75fafc0bbdba6a822aab0de5d7e714424
vixrodriguez <206ultimate>
2022-06-28 20:15:59 -0500

    [ADD] Amazon SES Example

---
+++ backend/zebrands_api/src/app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5433/zebrands"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Brands(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    products = db.relationship("Products")
+
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    sku = db.Column(db.String)
+    price = db.Column(db.Float, nullable=False)
+    brand_id = db.Column(db.Integer, db.ForeignKey("brands.id"))
+
+
+class UsersTypes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    user_type_id = db.Column(db.Integer, db.ForeignKey("users_types.id"))
+
+
+db.create_all()
