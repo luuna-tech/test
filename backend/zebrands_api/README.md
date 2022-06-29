6984e013baea29a8af07bd76e5408f3ca1cfca97
vixrodriguez <206ultimate>
2022-06-28 19:27:27 -0500

    [UPD] Remove sql script

---
+++ backend/zebrands_api/README.md
@@ -0,0 +1,12 @@
+<img src="https://zebrands.mx/wp-content/uploads/2021/07/WEB-ZEB-05-1-1024x291.png" width="300">
+
+### Technologies
+- Docker Compose
+- PostgreSQL
+- Python 3.8
+- Flask
+- SQLAlchemy
+- API REST
+- Amazon SES
+
+### Running
--- backend/zebrands_api/src/main.py
+++ backend/zebrands_api/src/main.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5433/zebrands"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
